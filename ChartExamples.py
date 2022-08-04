c6aa2f8f17db12017f4d262dc7364d833f659971
lipirika <lipirika@mail.ru>
2022-08-04 07:32:05 +0300

    Создание основы программы

---
+++ ChartExamples.py
@@ -0,0 +1,11 @@
+# Примеры построения графиков
+
+import tkinter as tk
+
+# Саздание главного окна
+window=tk.Tk()
+window.geometry("450x450")
+window.title("Примеры построения графиков")
+
+# Запуск цикла mainloop
+window.mainloop()
